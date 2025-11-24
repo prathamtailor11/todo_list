4af95c95c74eb208dcae57cea5f42f3c22d7e459
Pratham Tailor <prathamtailor11@gmail.com>
2025-11-24 21:54:50 +0530

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/todo_list/'
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo_list/',  // 👈 important
 })
